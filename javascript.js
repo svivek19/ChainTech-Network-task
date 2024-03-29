7672cf42abd4a6faec7bb7f9ef3e0af6561b047b
svivek19 <svivek030503@gmail.com>
2024-03-29 12:49:54 +0530

    task finish

---
+++ javascript.js
@@ -0,0 +1,14 @@
+//Made By: Krisha Botadara
+
+// Function to open the modal
+function openModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "block";
+  }
+  
+  // Function to close the modal
+  function closeModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+  }
+  
